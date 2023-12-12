da280ef3a0e85fb827d70d9683c69beee116de6a
viveksreenath <sreenathvivek@gmail.com>
2023-12-12 17:05:31 +0530

    Counter - second commit

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,6 +2,7 @@ import "./card.css";
 const Card = (props) => {
   return (
     <div className="card">
+      <p>Card</p>
       <img src={props.image} className="imgclass" />
       <h1>{props.title}</h1>
       <p>{props.description}</p>
